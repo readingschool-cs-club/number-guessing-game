f9e11534cb120061050623ffa011fb86afabd84b
Rehan Hafeez <badgerboy1002@gmail.com>
2015-03-16 16:27:19 +0000

    bug fix (defined "guess")

--- guess.py
+++ guess.py
@@ -8,6 +8,7 @@ maximum_guesses = 10
 number_to_guess = input("Please enter a number: ")
 
 guess_taken = 0
+guess = ""
 while guess != number_to_guess and guesses_taken < maximum_guesses:
   guesses_taken += 1
   guess = input("Please enter your guess: ")
