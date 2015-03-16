47a07a183794babf0f63150869e1cb2b7227c3fa
Rehan Hafeez <badgerboy1002@gmail.com>
2015-03-16 16:29:15 +0000

    damn my spelling is bad (Raehik's spelling)!

--- guess.py
+++ guess.py
@@ -7,7 +7,7 @@ maximum_guesses = 10
 
 number_to_guess = input("Please enter a number: ")
 
-guess_taken = 0
+guesses_taken = 0
 guess = ""
 while guess != number_to_guess and guesses_taken < maximum_guesses:
   guesses_taken += 1
